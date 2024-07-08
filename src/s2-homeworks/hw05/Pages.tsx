6c928dc9e7627b6e58dcd330d1ecee142ee8d7e8
Veta-s <112316829+Veta-s@users.noreply.github.com>
2024-07-08 18:59:47 +0300

    hw3.1 debug

--- src/s2-homeworks/hw05/Pages.tsx
+++ src/s2-homeworks/hw05/Pages.tsx
@@ -16,25 +16,19 @@ function Pages() {
         <div>
             {/*Routes выбирает первый подходящий роут*/}
             <Routes>
-                return (
-                <div>
-                    {/*Routes выбирает первый подходящий роут*/}
-                    <Routes>
-                        {/* в начале мы попадаем на страницу '/' и переходим сразу на страницу /pre-junior*/}
-                        <Route path="/" element={<Navigate to={PATH.PRE_JUNIOR} />} />
+                {/* в начале мы попадаем на страницу '/' и переходим сразу на страницу /pre-junior*/}
+                <Route path="/" element={<Navigate to={PATH.PRE_JUNIOR} />} />
 
-                        {/* роуты для /pre-junior, /junior, /junior-plus */}
-                        <Route path={PATH.PRE_JUNIOR} element={<PreJunior />} />
-                        <Route path={PATH.JUNIOR} element={<Junior />} />
-                        <Route path={PATH.JUNIOR_PLUS} element={<JuniorPlus />} />
+                {/* роуты для /pre-junior, /junior, /junior-plus */}
+                <Route path={PATH.PRE_JUNIOR} element={<PreJunior />} />
+                <Route path={PATH.JUNIOR} element={<Junior />} />
+                <Route path={PATH.JUNIOR_PLUS} element={<JuniorPlus />} />
 
-                        {/* роут для несуществующей страницы должен отрисовать <Error404 /> */}
-                        <Route path="*" element={<Error404 />} />
-                    </Routes>
-                </div>
-                );
+                {/* роут для несуществующей страницы должен отрисовать <Error404 /> */}
+                <Route path="*" element={<Error404 />} />
             </Routes>
         </div>
+
     )
 }
 
