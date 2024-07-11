dcdba920ce09bb22628f41cff1534267904dbb94
Veta-s <112316829+Veta-s@users.noreply.github.com>
2024-07-11 18:04:08 +0300

    hw3.4 init

--- src/s2-homeworks/hw08/bll/homeWorkReducer.ts
+++ src/s2-homeworks/hw08/bll/homeWorkReducer.ts
@@ -4,17 +4,19 @@ type ActionType =
     | { type: 'sort'; payload: 'up' | 'down' }
     | { type: 'check'; payload: number }
 
-export const homeWorkReducer = (state: any, action: any): any => { // need to fix any
+export const homeWorkReducer = (state: Array<UserType>, action: ActionType): Array<UserType> => { // need to fix any
     switch (action.type) {
-        case 'sort': { // by name
+        case 'sort':
+            if (action.payload === 'up') {
+                return state.slice().sort((a, b) => a.name.localeCompare(b.name));
+            } else {
+                return state.slice().sort((a, b) => b.name.localeCompare(a.name));
+            }
 
-            return state // need to fix
-        }
-        case 'check': {
+        case 'check':
+            return state.filter(user => user.age >= action.payload);
 
-            return state // need to fix
-        }
         default:
-            return state
+            return state;
     }
 }
